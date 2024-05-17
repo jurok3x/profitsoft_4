fb07e8aafcf88e8a7f5a15034b9288a9e2ee3300
ykotsiuba <jurok3x@gmail.com>
2024-05-17 11:46:35 +0300

    Added endpoints.

--- src/routers/comments/index.ts
+++ src/routers/comments/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import {
+  findCommentsByArticleId,
+  findCommentsCount,
   saveComment,
 } from 'src/controllers/comments';
 
 const router = express.Router();
 
 router.post('', saveComment);
+router.post('/_counts', findCommentsCount);
+router.get('', findCommentsByArticleId);
 
 export default router;
