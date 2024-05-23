9b766f864ff21ceb13b8d39b087c8172e2c74e0d
ykotsiuba <jurok3x@gmail.com>
2024-05-23 15:32:11 +0300

    Updated types.

---
+++ src/comments/types/types.ts
@@ -0,0 +1,8 @@
+export { type ArticleCommentCountDto } from "./articleCommentCountDto.type";
+export { type CommentCountRequestDto } from "./commentCountRequestDto.type";
+export { type CommentCountResponseDto } from "./commentCountResponseDto.type";
+export { type CommentDto } from "./commentDto.type";
+export { type CommentListRequestDto } from "./commentListRequestDto.type";
+export { type CommentSaveDto } from "./commentSaveDto.type";
+export { type FindByArticleIdRequestDto } from "./findByArticleIdRequestDto.type";
+
