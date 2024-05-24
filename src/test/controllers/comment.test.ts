579b5c79e17e4aa18e618fc7bcf300dfb219768e
ykotsiuba <jurok3x@gmail.com>
2024-05-24 15:25:19 +0300

    Improved tests.

--- src/test/controllers/comment.test.ts
+++ src/test/controllers/comment.test.ts
@@ -2,11 +2,13 @@ import bodyParser from 'body-parser';
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import express from 'express';
+import httpStatus from 'http-status';
 import sinon from 'sinon';
+import type { ArticleClient } from 'src/client/articleClient';
+import { articleClient } from 'src/client/client';
 import Comment from 'src/comments/comment.model';
 import type { CommentSaveDto } from 'src/comments/types/commentSaveDto.type';
 import routers from 'src/routers/comments';
-import * as commentService from 'src/services/comments';
 import { comments, seed } from '../config/commentsConfig';
 import mongoSetup from '../mongoSetup';
 
@@ -17,20 +19,23 @@ const { expect } = chai;
 chai.use(chaiHttp);
 chai.should();
 
-const sandbox = sinon.createSandbox();
-
 const app = express();
 
 app.use(bodyParser.json({ limit: '1mb' }));
 app.use('/', routers);
 
 describe('Comment controller', () => {
+    let clientStub: sinon.SinonStubbedInstance<ArticleClient>;
+    
     before(async () => {
         await mongoSetup;
         await seed();
     });
+    beforeEach(() => {
+        clientStub = sinon.stub(articleClient);
+    });
     afterEach(() => {
-        sandbox.restore();
+        sinon.restore();
     });
 
     it('should save the comment', (done) => {
@@ -40,24 +45,63 @@ describe('Comment controller', () => {
             articleId: '7f94d3da-a46a-4a90-a3d3-edbe4311dd83',
         }
 
-        const articleExistsStub = sandbox.stub(commentService, 'checkArticleExists');
-        articleExistsStub.resolves(true);
+        clientStub.checkArticleExists.resolves(true);
 
         chai.request(app)
             .post('')
             .send(request)
             .end(async (_, res) => {
                 await Comment.findByIdAndDelete(res.body._id);
-                res.should.have.status(201);
+                res.should.have.status(httpStatus.CREATED);
+                expect(res.body).to.have.property('_id').that.is.a('string');
                 expect(res.body).to.have.property('text').that.is.a('string');
                 expect(res.body).to.have.property('author').that.is.a('string');
-                sandbox.assert.calledOnce(articleExistsStub);
+            
+                done();
+            })
+    });
+
+    it('should fail saving comment when article not found', (done) => {
+        const request: CommentSaveDto = {
+            text: 'Great!',
+            author: 'John Doe',
+            articleId: '7f94d3da-a46a-4a90-a3d3-edbe4311dd83',
+        }
+
+        clientStub.checkArticleExists.resolves(false);
+
+        chai.request(app)
+            .post('')
+            .send(request)
+            .end(async (_, res) => {
+                res.should.have.status(httpStatus.BAD_REQUEST);
+                expect(res.body).to.have.property('message').eql(`Article with ID ${request.articleId} does not exist`);
+            
+                done();
+            })
+    });
+
+    it('should fail saving comment when author is blank', (done) => {
+        const request: CommentSaveDto = {
+            text: 'Great!',
+            author: ' ',
+            articleId: '7f94d3da-a46a-4a90-a3d3-edbe4311dd83',
+        }
+
+        clientStub.checkArticleExists.resolves(true);
+
+        chai.request(app)
+            .post('')
+            .send(request)
+            .end(async (_, res) => {
+                res.should.have.status(httpStatus.BAD_REQUEST);
+                expect(res.body).to.have.property('message').eql(`Comment validation failed: author:   should not be empty`);
             
                 done();
             })
     });
     
-    it('should list the comments', (done) => {
+    it('should return list of comments when find by id', (done) => {
         const response = data.map(item => ({
             _id: item.id,
             text: item.text,
@@ -80,6 +124,18 @@ describe('Comment controller', () => {
             });
     });
 
+    it('should throw error when article id undefined', (done) => {
+
+        chai.request(app)
+            .get('')
+            .end((_, res) => {
+                res.should.have.status(400);
+                expect(res.body).to.have.property('message').eql('Article ID must be defined.');
+        
+                done();
+            });
+    });
+
     it('should return the comments count', (done) => {
         const request = {
             "articleIds": ["7f94d3da-a46a-4a90-a3d3-edbe4311dd83", "7f94d3da-a46a-4a90-a3d3-edbe4311dd82"]
@@ -92,6 +148,18 @@ describe('Comment controller', () => {
                 res.should.have.status(200);
                 expect(res.body).to.have.property('7f94d3da-a46a-4a90-a3d3-edbe4311dd82').that.is.a('number');
                 expect(res.body).to.have.property('7f94d3da-a46a-4a90-a3d3-edbe4311dd83').that.is.a('number');
+        
+                done();
+            });
+    });
+
+    it('should throw error when request list is not an array', (done) => {
+        chai.request(app)
+            .post('/_counts')
+            .send('definitely not an array')
+            .end((_, res) => {
+                res.should.have.status(400);
+                expect(res.body).to.have.property('message').eql('Invalid articleIds format. Must be an array of strings.');
         
                 done();
             });
