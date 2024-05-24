a76081073cc9688ec9aad36410a8141c78f608b9
ykotsiuba <jurok3x@gmail.com>
2024-05-24 15:45:27 +0300

    Added docs.

--- src/comments/interfaces/ICommentRepo.ts
+++ src/comments/interfaces/ICommentRepo.ts
@@ -7,7 +7,24 @@ import type {
 } from "../types/types.ts";
 
 export interface ICommentRepo {
+    /**
+     * Finds comments by article ID with pagination and sorting.
+     * @param {FindByArticleIdRequestDto} params - The parameters for finding comments.
+     * @returns {Promise<IComment[]>} The comments.
+     */
     findByArticleId(data: FindByArticleIdRequestDto): Promise<IComment[]>;
+
+    /**
+     * Saves a new comment.
+     * @param {CommentSaveDto} request - The request data for saving a comment.
+     * @returns {Promise<IComment>} The saved comment.
+     */
     save(comment: CommentSaveDto): Promise<IComment>;
+
+    /**
+     * Gets the count of comments for multiple articles.
+     * @param {CommentCountRequestDto} params - The array of articles id.
+     * @returns {Promise<ArticleCommentCountDto[]>} The comment counts.
+     */
     getCount(data: CommentCountRequestDto): Promise<ArticleCommentCountDto[]>;
 }
--- src/comments/interfaces/ICommentService.ts
+++ src/comments/interfaces/ICommentService.ts
@@ -7,9 +7,29 @@ import type {
 } from "../types/types.ts";
 
 export interface ICommentService {
+    /**
+     * Finds comments by article ID.
+     * @param {FindByArticleIdRequestDto} params - The parameters for finding comments includes articleId and pagination.
+     * @returns {Promise<CommentDto[]>} The comments.
+     * @throws {InternalError} If the article ID is not provided.
+     */
     findByArticleId(data: FindByArticleIdRequestDto): Promise<CommentDto[]>;
+
+    /**
+     * Saves a new comment.
+     * @param {CommentSaveDto} request - The request data for saving a comment.
+     * @returns {Promise<CommentDto>} The saved comment.
+     * @throws {InternalError} If the article does not exist or provided request data are not valid.
+     */
     save(
         request: CommentSaveDto,
     ): Promise<CommentDto>;
+
+    /**
+     * Gets the count of comments for multiple articles.
+     * @param {CommentCountRequestDto} params - The string array of articles id.
+     * @returns {Promise<CommentCountResponseDto>} The comment counts.
+     * @throws {InternalError} If the request format is invalid.
+     */
     getCount(data: CommentCountRequestDto): Promise<CommentCountResponseDto>;
 }
